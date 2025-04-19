6f23bfdc55e507d921aa3d5caeeb9a0825db5380
Stedman Dennis <stedmandennis@gmail.com>
2025-04-18 19:28:11 -0500

    Use formatters to in print_progress_bar

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 def print_progress_bar(iteration: int, total: int, length: int=50):
-    percent = 100 * (iteration / total)
-    filled_length = length * iteration // total
+    progress = iteration / total
+    filled_length = (length * iteration) // total
     bar = '█' * filled_length + '-' * (length - filled_length)
-    print(f'\r|{bar}| {percent:.1f}% Complete ({iteration} of {total})', end='', flush=True)
+    print(f'\r|{bar}| {progress:{".2%"}} Complete ({iteration:,} of {total:,})', end='', flush=True)
     if iteration == total:
         print()
